d0fb3649444dd9a5dbb07679489748cb87cac7ca
just.sample@mail.ru <just.sample@mail.ru>
2022-05-13 16:54:29 +0700

    Добавил начало для добавления программ

--- WPF_Main/ViewModel/ProgramAdderViewModel.cs
+++ WPF_Main/ViewModel/ProgramAdderViewModel.cs
@@ -17,8 +17,90 @@ namespace WPF_Main.ViewModel
     public class ProgramAdderViewModel : ViewModelBase
     {
 
+        private string _nameProgram;
+        public string NameProgram
+        {
+            get => _nameProgram;
+
+            set
+            {
+                if (_nameProgram == value) return;
+                _nameProgram = value;
+                RaisePropertyChanged(nameof(NameProgram));
+            }
+        }
+
+        private Programs _selectedProgram;
+        public Programs SelectedProgram
+        {
+            get => _selectedProgram;
+
+            set
+            {
+                if(value == _selectedProgram) return;
+                _selectedProgram = value;
+                RaisePropertyChanged(nameof(SelectedProgram));
+            }
+        }
+
+        public ObservableCollection<Programs> Programs { get; set; }
+
         public ProgramAdderViewModel()
         {
+            using(error_collectorContext context = new error_collectorContext())
+            {
+                Programs = new ObservableCollection<Programs>(context.Programs);
+            }
+        }
+
+
+        public RelayCommand AddProgram
+        {
+            get => new RelayCommand(() =>
+            {
+                using(error_collectorContext context = new error_collectorContext())
+                {
+                    if (IsNullOrEmptyName())
+                    {
+                        return;
+                    }
+                }
+            });
+        }
+
+        public RelayCommand ChangeProgram
+        {
+            get => new RelayCommand(() =>
+            {
+                using (error_collectorContext context = new error_collectorContext())
+                {
+
+                }
+            });
+        }
+
+        public RelayCommand DeleteProgram
+        {
+            get => new RelayCommand(() =>
+            {
+                using (error_collectorContext context = new error_collectorContext())
+                {
+
+                }
+            });
+        }
+
+        public RelayCommand Exit
+        {
+            get => new RelayCommand(() =>
+            {
+
+            });
+        }
+
+        private bool IsNullOrEmptyName()
+        {
+            return string.IsNullOrWhiteSpace(NameProgram);
 
         }
 
